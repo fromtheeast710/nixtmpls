adde8fa6ac8cb1489b6e5a9b217c47a1497b84bc
fromtheeast710 <theeast710@proton.me>
2024-08-20 00:07:51 +1000

    Update Tauri and add OCaml

---
+++ OCaml/flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Nix Flake for OCaml development";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs { inherit system; };
+  in with pkgs; {
+    devShells.${system}.default = mkShell {
+      packages = [ ocaml ocamlformat ] ++ (with ocamlPackages; [ dune_3 odoc ]);
+    };
+  };
+}
